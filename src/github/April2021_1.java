e2808a09592d15ca47d4ba8a7f62eff3ece18160
Expert <Expert@DESKTOP-4K5QPJK>
2021-08-01 05:37:50 -0500

    [pravin]sample program

---
+++ src/github/April2021_1.java
@@ -0,0 +1,19 @@
+package github;
+
+public class April2021_1 
+{
+
+	static
+	{
+		System.out.println("My name is \"Pravin Dhole\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	
+	
+	
+}
